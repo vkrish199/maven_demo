a6f1d239651705ae1956bc1b4a6802b40e65ec20
Varun K <varun.krishnamurthy@yahoo.com>
2025-08-04 14:25:58 +0530

    Maven demo

---
+++ src/main/java/com/example/maven_project/App.java
@@ -0,0 +1,14 @@
+package com.example.maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = 6;
+        System.out.println( "Hello World! from varun : " + a);
+    }
+}
